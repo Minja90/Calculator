563122eb531c8fc3916b1b23b7475c8e8c608754
Minja Marcetic <minja90@live.com>
2015-08-13 22:30:07 +0200

    Updating

--- app/src/main/java/com/etf/telekomunikacije/calculator/GlavniActivity.java
+++ app/src/main/java/com/etf/telekomunikacije/calculator/GlavniActivity.java
@@ -2,6 +2,7 @@ package com.etf.telekomunikacije.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,8 +35,57 @@ public class GlavniActivity extends Activity {
         podeljeno = (Button)findViewById(R.id.podeljeno);
         textField = (EditText) findViewById(R.id.textField);
 
+    }
 
 
+    public void klikJedan(View view){
+        textField.setText("1");
+        textField.invalidate();
+    }
+
+    public void klikDva(View view){
+        textField.setText("2");
+
+    }
+    public void klikTri(View view){
+        textField.setText("3");
+    }
+    public void klikCetiri(View view){
+        textField.setText("4");
+    }
+    public void klikPet(View view){
+        textField.setText("5");
+    }
+    public void klikSest(View view){
+        textField.setText("6");
+    }
+    public void klikSedam(View view){
+        textField.setText("7");
+    }
+    public void klikOsam(View view){
+        textField.setText("8");
+    }
+    public void klikDevet(View view){
+        textField.setText("9");
+    }
+    public void klikNula(View view){
+        textField.setText("0");
+    }
+    public void klikPlus(View view){
+        textField.setText("+");
+    }
+    public void klikMinus(View view){
+        textField.setText("-");
+    }
+    public void klikPuta(View view){
+        textField.setText("*");
+    }
+    public void klikPodeljeno(View view){
+        textField.setText("/");
+    }
+    public void klikBrisi(View view){
+        textField.setText("");
+        textField.invalidate();
     }
 
 
